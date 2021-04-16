95f3a8d9dfc011075278da351d2c31e42c34987e
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-04-16 21:53:57 +0300

    Adds home page

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import style from "./HomePage.module.css";
 
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <div className={style.homePage}>
+      <h1 className={style.homeTitle}>Home page of phonebook</h1>
+    </div>
+  );
 };
 
 export default HomePage;
